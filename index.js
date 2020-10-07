bbe62b337eb1e50609c49fe2015279857efcfc59
Adrian Barros <adriansbarros1@gmail.com>
2020-10-06 23:09:11 -0400

    set static file

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-});
 
 const PORT = process.env.PORT || 5000;
 
