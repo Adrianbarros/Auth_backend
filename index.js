dd32e0ec0f3d4b9288f3a43e049c6377494bec6a
Adrian Barros <adriansbarros1@gmail.com>
2020-10-07 22:17:50 -0400

    about

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 //set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+//setting the port
 const PORT = process.env.PORT || 5000;
-
+//start the server in the given port
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
