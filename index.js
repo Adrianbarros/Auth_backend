8410cb01b14efeb1f4d28c99c5027207929b64d7
Adrian Barros <adriansbarros1@gmail.com>
2020-10-14 22:56:30 -0400

    sample route

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
+
+const members = {
+    id: 1,
+    name: 'name'
+}
+
+app.get('api/members', (req, res) => {
+    res.json(members);
+});
+
 //set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
