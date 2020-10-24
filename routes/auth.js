97af55d7f43f55133cbbe195788603c2ac5d1212
Adrian Barros <adriansbarros1@gmail.com>
2020-10-23 22:41:07 -0400

    login validation

--- routes/auth.js
+++ routes/auth.js
@@ -30,6 +30,20 @@ router.post('/register', async (req, res) => {
         res.status(400).send(err);
     }
 });
+router.post('/login', async (req, res) => {
+    //validation
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+
+    //Check if the email  doest exists
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) return res.status(400).send('Email or Password is not Correct')
+    //Check if password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if (!validPass) return res.status(400).send('Email or Password is not Correct')
+
+    res.send('logged in!')
+});
 
 
 
